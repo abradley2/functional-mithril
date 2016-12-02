5f10b5629bf5c163fa97ff4f5a4452210faef327
Tony <tony@localhost.localdomain>
2016-12-01 22:29:02 -0500

    fied this crap

--- src/main.js
+++ src/main.js
@@ -3,20 +3,13 @@ const R = require('ramda')
 
 const TodoList = require('./components/TodoList')
 
-function todoRoute () {
-	console.log('todos')
-	return {view: TodoList}
-}
-
-console.log('DOIN IT')
-
 document.addEventListener('DOMContentLoaded', function () {
 
 	m.route.mode = 'pathname'
 
-	m.route(document.body, '/', {
-		'': todoRoute,
-		'/:filter': todoRoute
+	m.route(document.getElementById('app'), '/', {
+		'/': {view: TodoList},
+		'/:filter': {view: TodoList}
 	})
 
 })
