930afabe381f866f28d4ddb4dae6d616b20c6b16
Anthony Bradley <anthonybradley@UPSIDE-2334.fios-router.home>
2016-12-11 20:04:34 -0500

    smarter links

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,15 +3,31 @@ const R = require('ramda')
 const store = require('../store')
 const constants = require('../constants')
 
+const actions = {
+	route: function (href, replace) {
+		store.dispatchAction({
+			type: constants.ROUTE,
+			href: href,
+			replace: replace
+		})
+	},
+	clearViewState: function () {
+		store.dispatchAction({
+			type: constants.CLEAR_VIEW_STATE
+		})
+	}
+}
+
 function Link (ctrl, args, children) {
 	return m('a' + (args.className || ''), R.merge({
 		config: function () {
 			if (args.href.indexOf('javascript:void(0)') === -1) return
-			store.dispatchAction({
-				type: constants.ROUTE,
-				href: args.href,
-				replace: args.replace || false
-			})
+
+			actions.route(args.href, args.replaceState || false)
+
+			if (args.clearViewState !== false) {
+				actions.clearViewState()
+			}
 		}
 	}, args), args.innerText || children)
 }
